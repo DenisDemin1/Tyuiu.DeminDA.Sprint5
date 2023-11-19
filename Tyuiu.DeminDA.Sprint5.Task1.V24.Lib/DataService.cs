07de56febe5c1ef72a37616168b25f61d31cce7f
DeminDenis1 <maxwelldenis888@gmail.com>
2023-11-19 18:20:01 +0500

    Task1

---
+++ Tyuiu.DeminDA.Sprint5.Task1.V24.Lib/DataService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.DeminDA.Sprint5.Task1.V24.Lib
+{
+    public class DataService : ISprint5Task1V24
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            if (fileExist)
+            {
+                File.Delete(path);
+            }
+
+            double y;
+            string strY;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double det = 4 * x - 0.5;
+                if (det == 0)
+                {
+                    return Convert.ToString(0);
+                }
+                else
+                {
+                    y = Math.Round(3* Math.Cos(x) / (4 * x - 0.5) + Math.Sin(x) - 5 * x - 2,2);
+                }
+                strY = Convert.ToString(y);
+
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, strY+Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, strY);
+                }
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.DeminDA.Sprint5.Task1.V24/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+using Tyuiu.DeminDA.Sprint5.Task1.V24.Lib;
+using System.IO;
+
+namespace Tyuiu.DeminDA.Sprint5.Task1.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("startValue = "+ startValue);
+            Console.WriteLine("stiopValue = "+ stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
