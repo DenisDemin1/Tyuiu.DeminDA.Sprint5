c275fc997c1e37ad6980b795bd83f7d9cc2882b6
DeminDenis1 <maxwelldenis888@gmail.com>
2023-11-26 20:11:01 +0500

    Task7

---
+++ Tyuiu.DeminDA.Sprint5.Task7.V23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DeminDA.Sprint5.Task7.V23.Lib;
+using System.IO;
+
+namespace Tyuiu.DeminDA.Sprint5.Task7.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask7V23.txt";
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutDataFileTask7V23.txt";
+
+
+            Console.WriteLine("Данные находяться в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+            Console.WriteLine("Находится в файле : ");
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
+
+        }
+    }
+}
